08f4a6107ea7ab7f3f86151c2ca63c86093443eb
Harold Martinez <hanthonym@outlook.com>
2016-02-24 09:35:42 -0400

    Validate phone input

--- src/Windows/Findier.Windows/ViewModels/NewPostViewModel.cs
+++ src/Windows/Findier.Windows/ViewModels/NewPostViewModel.cs
@@ -18,6 +18,11 @@ namespace Findier.Windows.ViewModels
                 "^((([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+(\\.([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?$");
 
         private readonly IFindierService _findierService;
+
+        private readonly Regex _phoneRegex =
+            new Regex(
+                "^(\\+\\s?)?((?<!\\+.*)\\(\\+?\\d+([\\s\\-\\.]?\\d+)?\\)|\\d+)([\\s\\-\\.]?(\\(\\d+([\\s\\-\\.]?\\d+)?\\)|\\d+))*(\\s?(x|ext\\.?)\\s?\\d+)?$");
+
         private bool _canMessage = true;
         private string _email;
 
@@ -214,13 +219,16 @@ namespace Findier.Windows.ViewModels
                 return;
             }
 
-            if (!string.IsNullOrWhiteSpace(Email))
+            if (!string.IsNullOrWhiteSpace(Email) && !_emailRegex.IsMatch(Email))
             {
-                if (!_emailRegex.IsMatch(Email))
-                {
-                    CurtainPrompt.ShowError("Please enter a valid email.");
-                    return;
-                }
+                CurtainPrompt.ShowError("Please enter a valid email.");
+                return;
+            }
+
+            if (!string.IsNullOrWhiteSpace(PhoneNumber) && !_phoneRegex.IsMatch(PhoneNumber))
+            {
+                CurtainPrompt.ShowError("Please enter a valid phone number.");
+                return;
             }
 
             if (!CanMessage && string.IsNullOrWhiteSpace(Email)
